9c7d0669be18f3f71e3e20e1fff96858fd0d48c5
kshitij <kshitij@something.com>
2019-07-21 22:49:02 +0530

    'assignment1solution'

---
+++ mod1sol/app.js
@@ -0,0 +1,20 @@
+( function () {
+  'use strict';
+  angular.module('LunchChecker',[])
+.controller('LunchCheckerController',LunchCheckerController);
+LunchCheckerController.$inject =['$scope'];
+function LunchCheckerController($scope)
+{$scope.dishes= "";
+$scope.msg ="";
+$scope.check= function(){
+  var no_of_dishes = $scope.dishes.split(',');
+  if(no_of_dishes==0)
+{  $scope.msg="Please enter data first";}
+  else if (no_of_dishes.length <= 3 )
+  {  $scope.msg="Enjoy!";}
+    else{
+    $scope.msg="Too much!";
+  }
+};
+}
+})();
